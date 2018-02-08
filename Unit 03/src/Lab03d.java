a4394561d275d038bd47b63376e612c6ce24e461
sunw2347 <williamsun2347@gmail.com>
2018-02-08 10:52:35 -0800

    Unit 3 Update

--- Unit 03/src/Lab03d.java
+++ Unit 03/src/Lab03d.java
@@ -13,10 +13,45 @@ public class Lab03d
 	public static void main( String[] args )
 	{
 		//add test cases
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter x1 :: \n");
+		int x1 = keyboard.nextInt();
+		System.out.println("Enter x2 :: \n");
+		int x2 = keyboard.nextInt();
+		System.out.println("Enter y1 :: \n");
+		int y1 = keyboard.nextInt();
+		System.out.println("Enter y2 :: \n");
+		int y2 = keyboard.nextInt();
 		
+		Distance test = new Distance(x1, x2, y1, y2);
+		test.calcDistance();
+		test.print();
 		
+		System.out.println("Enter x1 :: \n");
+		x1 = keyboard.nextInt();
+		System.out.println("Enter x2 :: \n");
+		x2 = keyboard.nextInt();
+		System.out.println("Enter y1 :: \n");
+		y1 = keyboard.nextInt();
+		System.out.println("Enter y2 :: \n");
+		y2 = keyboard.nextInt();
 		
+		test.setCoordinates(x1, y1, x2, y2);
+		test.calcDistance();
+		test.print();
 		
+		System.out.println("Enter x1 :: \n");
+		x1 = keyboard.nextInt();
+		System.out.println("Enter x2 :: \n");
+		x2 = keyboard.nextInt();
+		System.out.println("Enter y1 :: \n");
+		y1 = keyboard.nextInt();
+		System.out.println("Enter y2 :: \n");
+		y2 = keyboard.nextInt();
+		
+		test.setCoordinates(x1, y1, x2, y2);
+		test.calcDistance();
+		test.print();
 			
 	}
 }
