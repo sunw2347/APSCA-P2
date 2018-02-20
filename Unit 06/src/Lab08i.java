a07e69d81d6dbbc49c2e37b57e868b8d6e2f69da
sunw2347 <williamsun2347@gmail.com>
2018-02-20 10:08:32 -0800

    Unit 6 (Lab 08i)
    Due 2/20/18

---
+++ Unit 06/src/Lab08i.java
@@ -0,0 +1,53 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.lang.Math;
+
+public class Lab08i
+{
+	public static void main ( String[] args )
+	{
+		//add test cases
+		Prime test = new Prime();
+		test.setPrime(24);
+		test.isPrime();
+		test.toString();
+		
+		test.setPrime(7);
+		test.isPrime();
+		test.toString();
+		
+		test.setPrime(100);
+		test.isPrime();
+		test.toString();
+		
+		test.setPrime(113);
+		test.isPrime();
+		test.toString();
+		
+		test.setPrime(65535);
+		test.isPrime();
+		test.toString();
+		
+		test.setPrime(2);
+		test.isPrime();
+		test.toString();
+		
+		test.setPrime(7334);
+		test.isPrime();
+		test.toString();
+		
+		test.setPrime(7919);
+		test.isPrime();
+		test.toString();
+		
+		test.setPrime(1115125003);
+		test.isPrime();
+		test.toString();
+		
+	}	
+}
